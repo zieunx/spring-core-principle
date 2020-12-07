6b711e19f33743544e0be09a542d1aa2ff5370db
zieunx <wldms_1217@naver.com>
2020-12-08 00:15:27 +0900

    생성자 주입

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+// AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 '생성자를 통해서 주입(연결)'해준다.
+public class AppConfig {
+
+    // 생성자 주입
+    // 생성자를 통해 객체를 주입한다. (DIP를 지킬 수 있다.)
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+}
