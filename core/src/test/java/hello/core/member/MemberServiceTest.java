6d74511b6f9364f0ce12f44043b5ffec56712a3c
zieunx <wldms_1217@naver.com>
2020-12-08 22:38:29 +0900

    의존관계 주입 (DI) - AppConfig를 통해 관심사 분리
    - AppConfig를 만들어 설정의 역할을 분리해냈다.
    - OrderServiceImpl 에서 정의되던 MemberRepository, DiscountPolicy를 AppConfig에서 생성된 객체를 참조하도록 주입했다.
    - 이를 의존성주입/의존관계주입(DI)라고 한다.

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    /* 추상화에도 의존, 구현체에도 의존 -> DIP 위반 */
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
