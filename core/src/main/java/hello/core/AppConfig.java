5ddb6cff418aaab31120a82b9d186c76b1385b1a
zieunx <wldms_1217@naver.com>
2020-12-22 22:24:47 +0900

    AppCongig 리팩토링
    - 중복 코드에 대한 제거와 '역할'과 '구현'에 대한 분리가 필요했다.
    - 리팩토링을 통해 전체 서비스가 한눈에 들어오고, 구현에 대해 수정이 발생할 때 수정에 용이해졌다.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,11 +17,21 @@ public class AppConfig {
     // 생성자를 통해 객체를 주입한다. (DIP를 지킬 수 있다.)
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    //
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public
+    DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
