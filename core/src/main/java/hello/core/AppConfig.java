a93f5b4500c1648e1ba50b4c67a4f781189f2a75
zieunx <wldms_1217@naver.com>
2020-12-22 22:35:59 +0900

    새로운 할인정책 적용
    - 이미 구성역할의 'AppConfig'를 만들어두었기 때문에, 아주 편리하게 정액할인정책에서 정률할인정책으로 변경하였다.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -29,9 +30,9 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public
-    DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
