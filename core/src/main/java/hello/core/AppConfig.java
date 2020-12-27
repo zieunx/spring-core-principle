58e1c85c492639726df56aaa23b0b63a4bd953bd
zieunx <wldms_1217@naver.com>
2020-12-27 16:13:00 +0900

    스프링 컨테이너 적용
    - @Configuration 어노테이션으로 설정 역할의 클래스를 지정해준다.
    - @Bean 어노테이션으로 스프링 컨테이너에 등록할 bean들을 설정해준다.
    - ApplicationContext
      * ApplicationContext는 인터페이스이다.
      * ApplicationContext는 스프링컨테이너다.
      - AnnotationConfigApplicationContext 구현체로 어노테이션 기반 어플리케이션 설정을 생성해주고, 기존에 작성한 AppConfig.class 를 파라피터로 전달한다.
      - ApplicationContext의 getBean 메소드를 통해 빈을 등록해준다.
        * getBean(이름, 클래스);

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의\ 실제 동작에 필요한 구현 객체를 생성한다.
 // AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 '생성자를 통해서 주입(연결)'해준다.
+@Configuration
 public class AppConfig {
 
-    // 생성자 주입
-    // 생성자를 통해 객체를 주입한다. (DIP를 지킬 수 있다.)
-
+    @Bean
+    // 컴파일할 때 스프링 컨테이너에 빈이 등록된다.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    //
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
