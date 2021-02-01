59a27d0b45a02cec7ef2153c987dca4a8e27ced0
zieunx <wldms_1217@naver.com>
2021-02-01 11:10:27 +0900

    조회 빈 2개 이상 - @Autowired 필드명 매칭

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,12 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan( // @Component 어노테이션이 붙은 클래스를 스캔하여  빈 등록해준다.
-//        basePackages = "hello.core.member", // 탐색할 패키지를 설정할 수 있다.
-//        basePackageClasses = AutoAppConfig.class, // 설정 클래스의 패키지의 경로에서 탐색한다.
-        // Q. 기본값은 무엇인가? -> @ComponentScan이 붙은 클래스의 경로부터 시작된다.
+@ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-        // 에제 데이터 유지를 위해 Configuration 어노테이션에 해당하는 클래스들은 제외해주었다.
 )
 public class AutoAppConfig {
 
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,23 +7,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-// final 을 가진 값으로 생성자를 만들어준다. (cmd + F12)
 @Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-    // @Autowired 보다 더 편리한 @RequiredArgsConstructor !!
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        /*
-        단일 책임 원칙을 잘 지켰기 때문에, Order 쪽에서는 discount 에 관련하여 전혀 관여하지 않는다.
-        */
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
