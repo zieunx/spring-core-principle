e11a21aa4da955678f2a7022e36f25b2580207ff
zieunx <wldms_1217@naver.com>
2021-02-01 11:18:33 +0900

    조회 빈 2개 이상 - @Primary 로 최우선 구현체 지정

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary // 여러가지 빈들 중에 해당 어노테이션을 최우선으로 선택한다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
